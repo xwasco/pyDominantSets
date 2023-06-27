5ba805581599e7f2252c883225b80747037cf311
Sebastiano Vascon <xwasco@users.noreply.github.com>
2023-06-27 16:09:45 +0200

    Update pyDominantSets.py

--- pyDominantSets.py
+++ pyDominantSets.py
@@ -1,5 +1,5 @@
 import numpy as np
-import time
+
 
 def repdyn(A, x=None, maxiters=100, epsilon=1e-8):
 
@@ -28,14 +28,12 @@ def getDSs(A, minsize=1, maxiters=1000, eps=1e-6):
     W = []
 
     while np.sum(A)>0:
-        t1 = time.perf_counter()
-        x, iter, cohes = repdyn(A, maxiters=maxiters)
-        t2 = time.perf_counter()
+        x, iters, cohes = repdyn(A, maxiters=maxiters)
 
         idx = np.where(x>=eps)
 
         C.append(tuple(V[idx]))
-        W.append([cohes, (t2 - t1)*1000])
+        W.append([cohes, iters])
 
         V = np.delete(V,idx)
         A = np.delete(A, idx, axis=0)
@@ -43,7 +41,7 @@ def getDSs(A, minsize=1, maxiters=1000, eps=1e-6):
 
     if minsize>0:
     
-        if minsize==1: # we still have isolated nodes assign each to a separate cluster
+        if minsize==1:
             for id in V:
                 C.append(tuple([id])) 
         else:
@@ -53,9 +51,3 @@ def getDSs(A, minsize=1, maxiters=1000, eps=1e-6):
                     W.pop(i[0])
 
     return C,W
-t1 = time.perf_counter()
-macs = graph.maximal_cliques(min=3)
-t2 = time.perf_counter()
-tbk = t2 - t1
-print(f'Search maximal cliques: %.2fms' % ((t2 - t1) * 1000))
-print(tds,tbk)
